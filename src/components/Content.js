3e447d0047dc0e9ed579ef46728eeeb00a1e9928
Brendt Bly <brendt.bly@outlook.com>
2017-12-04 13:45:00 -0600

    Hello world

---
+++ src/components/Content.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+
+export class Content extends Component {
+  render() {
+    return (
+        <div>
+    <p className="App-intro">Trying some stuff</p>
+    <h2>It is {new Date().toLocaleTimeString()}.</h2>
+    </div>
+);
+  }
+}
---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+export class Header extends Component {
+  render() {
+    return (
+      <div id="header">
+        <h1>Tinkering with React</h1>
+      </div>
+    );
+  }
+}
